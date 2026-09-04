f0f791ace4fe152d1e9c35c7fa0f5116a4a6f631
Michael from work <j.michael.chen@gmail.com>
2026-09-04 11:30:34 -0700

    Added usage instructions

--- solution.py
+++ solution.py
@@ -1,5 +1,13 @@
 import tester as tester
 
+##### INSTRUCTIONS
+# To run this file, run `python solution.py`
+# You can add test cases to ./tester.py by modifying test_cases which has a definition of:
+# test_cases = {"NAME": {"mat": [[]], "val": 0}}
+# NAME is the name of the test case
+# mat is the mxn matrix (dimensions don't need to be specified)
+# val is the expected number of submatrices that have all 1s
+
 class Solution:
     def sprint(self, s):
         if False: # CHANGE THIS TO TRUE FOR PRINTING LOGS
